4d8c77209ec951f722ba3ab35f848056be7331cf
Drew Guarnera <dguarnera@wooster.edu>
2022-02-25 10:03:30 -0500

    Add code examples for CSV and txt

---
+++ python/examples/csvfiles/csv_reading.py
@@ -0,0 +1,86 @@
+"""Demonstration of reading CSV files
+
+CSV (comma separate values) files are
+structured data files that allows for
+retrieval of data by indicating each data
+field using a specific character (a comma
+for CSV values). These can be a common
+format for many datasets especially when
+the data does not exhibit a hierarchical
+structure.
+
+Note that you will need to run this program
+from WITHIN this directory or you will be
+unable to open the file.
+
+You can find details about the csv module here:
+https://docs.python.org/3.9/library/csv.html
+"""
+
+
+# import statments allow you to use other Python
+# moddules from other files (Classes, functions, etc.)
+# You can think of these like the #include preprocessor
+# directives in C/C++
+import csv
+import os
+
+
+def processCSVFile(input_file_path, output_file_path):
+    """Read a comma separated values
+    (CSV) file
+
+    Args:
+        input_file_path (str): absolute path to a CSV file
+        output_file_path (str): absolute path to the outpufile
+    """
+
+    grade_distribution = {}
+
+    with open(input_file_path, "r") as csv_file:
+        # The csv module's reader object can take an
+        # input file in a comma separated format
+        csv_data = csv.reader(csv_file)
+        
+        # I'm using next to skips the csv header
+        # because I won't use this data
+        next(csv_data)
+
+        for line in csv_data:
+            if line[-1] in grade_distribution:
+                grade_distribution[line[-1]] += 1
+            else:
+                grade_distribution[line[-1]] = 1
+    
+    # The "w" puts open in write mode and allows for
+    # files to be created. There are other file modes
+    # such as "a" for append. You can find a full table
+    # of modes here:
+    # https://www.tutorialspoint.com/python/python_files_io.htm
+    with open(output_file_path, "w") as output_file:
+        output_file.write("Grade Distribution\n")
+        for key, value in grade_distribution.items():
+            output_file.write(f"{key}: {value}\n")
+
+
+def main():
+    # If this script in not run within this directory, we will not be
+    # able to find grades.csv via a relative path. Python can help us
+    # locate a path to the file so it doesn't matter what the current
+    # working directory is for our terminal.
+    #
+    # Locate the absolute path to this script
+    absolute_path_to_script = os.path.realpath(__file__)
+    # Identify the absolute path to the directory where this script
+    # resides
+    absolute_path_to_directory = os.path.dirname(absolute_path_to_script)
+    # Join the directory path with a filename for the intput using the
+    # correct path separators depending on the OS (Win/Unix).
+    input_file_path = os.path.join(absolute_path_to_directory, "grades.csv")
+    output_file_path = os.path.join(absolute_path_to_directory, "report.txt")
+    
+    processCSVFile(input_file_path, output_file_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/examples/textfiles/textfile_reading.py
@@ -0,0 +1,83 @@
+"""Demonstration of reading text files
+
+Plain text files can be used as unstructured
+data or for a variety of applications to create
+persistent storage for data.
+"""
+
+
+# import statments allow you to use other Python
+# moddules from other files (Classes, functions, etc.)
+# You can think of these like the #include preprocessor
+# directives in C/C++
+import os
+
+
+def parseLogEntry(entry):
+    """Parse the apache log file entry
+    for the following fields:
+    
+    "Client IP" - - ["Time of Request"] "Request Line" "Request Code" "Size of data"
+
+    Args:
+        entry (str): apache log entry
+    Return:
+        tuple: collection of the above fields
+    """
+
+    # Find will locate the first instance of a string
+    # within another string.
+    ip_address = entry[:entry.find(" ")]
+    date_time = entry[entry.find("[") + 1:entry.find("]")]
+    quote_index = entry.find('"') + 1
+    
+    # Find can be provided a starting to being the search
+    request = entry[quote_index:entry.find('"', quote_index)]
+    request_code, request_size = entry.split(" ")[-2:]
+
+    return (ip_address, date_time, request, request_code, request_size)
+
+
+def readApacheLogFile(file_path):
+    """Read a apache log file
+
+    Args:
+        file_path (str): path to an apache log file
+    """
+
+    # with can be used with resources to help with
+    # ensuring that closing a stream happens
+    # automatically. You can use the "as" statment
+    # at the end to give a variable name to the open
+    # resource to reference it withint the with
+    # block
+    # The "r" puts open in read mode. There are other
+    # file modes such as "a" for append. You can find
+    # a full table of modes here:
+    # https://www.tutorialspoint.com/python/python_files_io.htm
+    with open(file_path, "r") as apache_log_file:
+        for line in apache_log_file:
+            # The strip function with no parameters removes
+            # leading and trailing whitespace from a string
+            print(parseLogEntry(line.strip()))
+
+
+def main():
+    # If this script in not run within this directory, we will not be
+    # able to find grades.csv via a relative path. Python can help us
+    # locate a path to the file so it doesn't matter what the current
+    # working directory is for our terminal.
+    #
+    # Locate the absolute path to this script
+    absolute_path_to_script = os.path.realpath(__file__)
+    # Identify the absolute path to the directory where this script
+    # resides
+    absolute_path_to_directory = os.path.dirname(absolute_path_to_script)
+    # Join the directory path with a filename for the intput using the
+    # correct path separators depending on the OS (Win/Unix).
+    input_file_path = os.path.join(absolute_path_to_directory, "log_2_small.txt")
+    readApacheLogFile(input_file_path)
+
+
+if __name__ == "__main__":
+    main()
